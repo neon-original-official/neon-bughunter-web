0ead24c07cb87be40fe8df3ed2da2658a297c0f5
neon-original-official <83065241+neon-original-official@users.noreply.github.com>
2021-11-13 00:26:53 +0700

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,7 @@
 console.log("Neon Api");
 /*Founder chromenium google LLC neon corporation*/
 var GUI = lesion.getstorage("sesion0");
-var Getrandomnumber = sesion("#/@+_-:!",random)
-GUI.set(loadcookie("sesion2"),random)
+var Getrandomnumber = sesion("#/@+_-:!",random);
+GUI.set(loadcookie("sesion2"),random);
 /*Founder roblox and neon*/
+roblox.detecor("foundjs");
