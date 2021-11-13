93eea0d34c2d4b3acb5b67fd6f78585a1eee1cad
neon-original-official <83065241+neon-original-official@users.noreply.github.com>
2021-11-13 14:40:56 +0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,7 @@
 
+ //membuat objek elemen
+        var hasil = document.getElementById("hasil-output");
+
+        // menampilkan output ke elemen hasil
+        hasil.innerHTML = "<p>Aku suka Javascript</p>";
+console.log("Loading");
